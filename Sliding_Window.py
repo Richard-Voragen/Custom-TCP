27e7e8ea3dea9dbc2b16626ae0a78b41f8e7a965
Richard Voragen <richard24680123@gmail.com>
2023-11-29 20:18:56 -0800

    Part 2 fixed

--- Sliding_Window.py
+++ Sliding_Window.py
@@ -16,14 +16,12 @@ with open('send.txt', 'rb') as f:
     data = f.read()
 
 #sends out the next message
-def send_next_message(seq_id, acks):
+def send_next_message(seq_id):
     temp_id = seq_id + (WINDOW_SIZE * MESSAGE_SIZE)
-    acks[temp_id] = False
     if ((seq_id + (WINDOW_SIZE * MESSAGE_SIZE)) < len(data)):
         message = int.to_bytes(temp_id, SEQ_ID_SIZE, byteorder='big', signed=True) + data[temp_id : temp_id + MESSAGE_SIZE]
-
         udp_socket.sendto(message, ('localhost', 5001))
-    return (seq_id + MESSAGE_SIZE, acks)
+    return (seq_id + MESSAGE_SIZE)
 
 #resends the message at seq_id
 def resend_message(seq_id):
@@ -32,22 +30,18 @@ def resend_message(seq_id):
 
 def send_initial_messages():
     seq_id_tmp = 0
-    acks = {}
     messages = []
     for i in range(WINDOW_SIZE):
         # construct messages
         # sequence id of length SEQ_ID_SIZE + message of remaining PACKET_SIZE - SEQ_ID_SIZE bytes
         message = int.to_bytes(seq_id_tmp, SEQ_ID_SIZE, byteorder='big', signed=True) + data[seq_id_tmp : seq_id_tmp + MESSAGE_SIZE]
         messages.append((seq_id_tmp, message))
-        acks[seq_id_tmp] = False
         # move seq_id tmp pointer ahead
         seq_id_tmp += MESSAGE_SIZE
 
     # send messages
     for sid, message in messages:
         udp_socket.sendto(message, ('localhost', 5001))
-
-    return (acks)
 
 # create a udp socket
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
@@ -59,10 +53,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
     # start sending data from 0th sequence
     seq_id = 0
     StartThroughputTime = time.time()
-    acks = {}
     fast_retransmit = 0
 
-    acks = send_initial_messages()
+    send_initial_messages()
     while seq_id < len(data):        
         try:
             # wait for ack
@@ -70,27 +63,34 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
             
             # extract ack id
             ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big')
-            acks[ack_id] = True
             
-            if (ack_id != seq_id):
+            if (ack_id == seq_id):
                 fast_retransmit += 1
-            if (fast_retransmit == 3):
-                resend_message(seq_id)
+                if (fast_retransmit == 3):
+                    #print("Fast Retransmit")
+                    resend_message(seq_id)
+                    fast_retransmit = 0
+                elif (seq_id + MESSAGE_SIZE > len(data)):
+                    break
+            else:
                 fast_retransmit = 0
+                while (seq_id < ack_id):
+                    seq_id = send_next_message(seq_id)
 
-            while (acks[seq_id]):
-                seq_id, acks = send_next_message(seq_id, acks)
-                fast_retransmit = 0
         except socket.timeout:
             # no ack received, resend unacked message
+            #print("Socket Timeout")
             fast_retransmit = 0
             resend_message(seq_id)
+        if (seq_id + MESSAGE_SIZE > len(data)):
+            break
 
     # send final closing message
     udp_socket.sendto(int.to_bytes(-1, 4, signed=True, byteorder='big'), ('localhost', 5001))
     totalTime = (time.time() - StartThroughputTime)
+    totalPackages = int(len(data)/MESSAGE_SIZE) + (len(data) % MESSAGE_SIZE > 0)
 
     #print("Total time was : " + totalTime.__str__())
-    #print("Total amount of packets : " + (len(acks)-WINDOW_SIZE).__str__())
-    print("Average delay per packet was : " + (totalTime/(len(acks)-WINDOW_SIZE)).__str__() + " seconds")
+    #print("Total amount of packets : " + totalPackages.__str__())
+    print("Average delay per packet was : " + (totalTime/totalPackages).__str__() + " seconds")
     print("Throughput was : " + (len(data)/totalTime).__str__() + " bytes per second")
--- Stop_And_Wait.py
+++ Stop_And_Wait.py
@@ -16,12 +16,11 @@ with open('send.txt', 'rb') as f:
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
 
     # bind the socket to a OS port
-    udp_socket.bind(("0.0.0.0", 5000))
+    udp_socket.bind(("0.0.0.0", 5002))
     udp_socket.settimeout(1)
     
     # start sending data from 0th sequence
     seq_id = 0
-    delays_per_packet = []
     StartThroughputTime = time.time()
     while seq_id < len(data):
         
@@ -33,11 +32,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
         message = int.to_bytes(seq_id_tmp, SEQ_ID_SIZE, byteorder='big', signed=True) + data[seq_id_tmp : seq_id_tmp + MESSAGE_SIZE]
 
         udp_socket.sendto(message, ('localhost', 5001))
-        startTime = time.time()
 
         try:
             ack, _ = udp_socket.recvfrom(PACKET_SIZE)
-            delays_per_packet.append(time.time() - startTime)
+            ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big')
+            print(ack_id/1020)
         except:
             print("Packet Rejected")
             udp_socket.sendto(message, ('localhost', 5001))
@@ -47,8 +46,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
     # send final closing message
     udp_socket.sendto(int.to_bytes(-1, 4, signed=True, byteorder='big'), ('localhost', 5001))
     totalTime = (time.time() - StartThroughputTime)
+    totalPackages = int(len(data)/MESSAGE_SIZE) + (len(data) % MESSAGE_SIZE > 0)
 
     #print("Total time was : " + totalTime.__str__())
-    #print("Total amount of packets : " + (len(acks)-WINDOW_SIZE).__str__())
-    print("Average delay per packet was : " + (totalTime/(len(acks)-WINDOW_SIZE)).__str__() + " seconds")
+    #print("Total amount of packets : " + (len(data)/MESSAGE_SIZE).__str__())
+    print("Average delay per packet was : " + (totalTime/totalPackages).__str__() + " seconds")
     print("Throughput was : " + (len(data)/totalTime).__str__() + " bytes per second")
