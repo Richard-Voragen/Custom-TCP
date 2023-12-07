65b8dadcb3c4742bfe76e4829f3fd6598f85a84e
hjyeung0 <jhyeung@ucdavis.edu>
2023-12-06 17:13:17 -0800

    Updated file names to reflect project submission

--- sender_custom.py
+++ sender_custom.py
@@ -1,8 +1,6 @@
 import socket
+from datetime import datetime
 import time
-import threading
-import math
-from joblib import Parallel, delayed
 
 # total packet size
 PACKET_SIZE = 1024
@@ -10,72 +8,158 @@ PACKET_SIZE = 1024
 SEQ_ID_SIZE = 4
 # bytes available for message
 MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
+# total packets to send
+WINDOW_SIZE = 0
 
-def send_closing_message(seq_id):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-        udp_socket.bind(("0.0.0.0", 5000))
-        udp_socket.settimeout(0.5)
-
-        finalMessage = int.to_bytes(seq_id + MESSAGE_SIZE, SEQ_ID_SIZE, byteorder='big', signed=True)
-        udp_socket.sendto(finalMessage, ('localhost', 5001))
-
-        closingMessage = int.to_bytes(-1, SEQ_ID_SIZE, byteorder='big', signed=True) + b"==FINACK=="
-        udp_socket.sendto(closingMessage, ('localhost', 5001))    
+SSTHRESH = 100000
 
 # read data
-
-def send(packet, startTime):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-
-        # bind the socket to a OS port
-        if (startTime == 0):
-            udp_socket.bind(("0.0.0.0", threading.get_native_id()))
-            udp_socket.settimeout(0.5)
-            per_packet_delay = time.time()
-        else:
-            per_packet_delay = startTime
-        
-        # start sending data from 0th sequence
-        seq_id = packet*MESSAGE_SIZE
-
-
-        #print(packet)
-        math.ceil(len(data)/MESSAGE_SIZE)
-
-        # construct messages
-        # sequence id of length SEQ_ID_SIZE + message of remaining PACKET_SIZE - SEQ_ID_SIZE bytes
-        message = int.to_bytes(seq_id, SEQ_ID_SIZE, byteorder='big', signed=True) + data[seq_id : seq_id + MESSAGE_SIZE]
-
-        udp_socket.sendto(message, ('localhost', 5001))
-
-        try:
-            ack, _ = udp_socket.recvfrom(PACKET_SIZE)
-            per_packet_delay = time.time() - per_packet_delay
-            ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big')
-            #print("acked", ack_id/1020)
-        except:
-            #print("Packet Rejected")
-            per_packet_delay = send(packet, per_packet_delay)
-        
-        return per_packet_delay
-
-
 with open('file.mp3', 'rb') as f:
     data = f.read()
     data += (b'\x00'* (MESSAGE_SIZE - (len(data)%MESSAGE_SIZE)))
 
-StartThroughputTime = time.time()
-total_data = math.ceil(len(data)/MESSAGE_SIZE)
-results = Parallel(n_jobs=20)(delayed(send)(i, 0) for i in range(total_data))
 
-send_closing_message(math.ceil(len(data)/MESSAGE_SIZE))
+def get_initial_time(seq_id):
+    global per_packet_delay
+    max = 0
+    for i in range(10):
+        try:
+            # wait for ack
+            ack, _ = udp_socket.recvfrom(PACKET_SIZE)
+            per_packet_delay[seq_id] = time.time() - per_packet_delay[seq_id]
+            if (per_packet_delay[seq_id] > max):
+                max = per_packet_delay[seq_id]
+            seq_id = send_next_message(seq_id)
+        except socket.timeout:
+            # no ack received, resend unacked message
+            #print("Socket Timeout")
+            resend_message(seq_id)
+    max *= 1.25
+    #print(max)
+    return seq_id, max
 
-StartThroughputTime = time.time() - StartThroughputTime
-sum = 0
-for i in results:
-    sum += i
 
-throughput = len(data)/StartThroughputTime
-print('{:.2f},'.format(round(throughput, 2)))
-print('{:.2f},'.format(round((sum / len(results)), 2)))
-print('{:.2f}'.format(round(throughput/ (sum / len(results)), 2)))
+#sends out the next message for the sliding window
+def send_next_message(seq_id):
+    global per_packet_delay
+
+    temp_id = seq_id + (WINDOW_SIZE * MESSAGE_SIZE)
+    if ((seq_id + (WINDOW_SIZE * MESSAGE_SIZE)) < len(data)):
+        #print("SENT", temp_id/1020)
+        message = int.to_bytes(temp_id, SEQ_ID_SIZE, byteorder='big', signed=True) + data[temp_id : temp_id + MESSAGE_SIZE]
+        udp_socket.sendto(message, ('localhost', 5001))
+
+        per_packet_delay[temp_id] = time.time()
+    return (seq_id + MESSAGE_SIZE)
+
+#resends the message at seq_id
+def resend_message(seq_id):
+    message = int.to_bytes(seq_id, SEQ_ID_SIZE, byteorder='big', signed=True) + data[seq_id : seq_id + MESSAGE_SIZE]
+    udp_socket.sendto(message, ('localhost', 5001))
+
+def send_closing_message(seq_id):
+    finalMessage = int.to_bytes(seq_id + MESSAGE_SIZE, SEQ_ID_SIZE, byteorder='big', signed=True)
+    udp_socket.sendto(finalMessage, ('localhost', 5001))
+
+    closingMessage = int.to_bytes(-1, SEQ_ID_SIZE, byteorder='big', signed=True) + b"==FINACK=="
+    udp_socket.sendto(closingMessage, ('localhost', 5001))    
+
+def increase_window_size(seq_id):
+    global WINDOW_SIZE
+
+    send_next_message(seq_id)
+    WINDOW_SIZE += 1
+
+    #print("NEW WINDOW SIZE: ", WINDOW_SIZE)
+
+def reset_window_size():
+    global SSTHRESH, WINDOW_SIZE, waitTime
+    SSTHRESH = max(20, WINDOW_SIZE)
+    WINDOW_SIZE = int(WINDOW_SIZE/2)
+    waitTime = time.time()
+
+waitTime = time.time()
+# create a udp socket
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+
+    # bind the socket to a OS port
+    udp_socket.bind(("0.0.0.0", 5000))
+    udp_socket.settimeout(1)
+    
+    # start sending data from 0th sequence
+    seq_id = 0
+    temp_window_size_adder = 0.0
+    StartThroughputTime = time.time()
+    fast_retransmit = 0
+
+    per_packet_delay = {}
+    delay = 0.25
+
+    increase_window_size(seq_id)
+    seq_id, delay = get_initial_time(seq_id)
+    while seq_id < len(data):        
+        try:
+            # wait for ack
+            ack, _ = udp_socket.recvfrom(PACKET_SIZE)
+            
+            # extract ack id
+            ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big')
+            #print("RECV", ack_id/1020)
+            
+            if (ack_id == seq_id and (time.time()-waitTime) > 2):
+                fast_retransmit += 1
+                if (fast_retransmit == 3):
+                    #print("Fast Retransmit")
+                    reset_window_size()
+                    fast_retransmit = 0
+                    resend_message(seq_id)
+                elif (seq_id + MESSAGE_SIZE > len(data)):
+                    break
+            elif (ack_id != seq_id):
+                fast_retransmit = 0
+                while (seq_id < ack_id):
+                    per_packet_delay[seq_id] = time.time() - per_packet_delay[seq_id]
+                    seq_id = send_next_message(seq_id)
+                if per_packet_delay[seq_id-MESSAGE_SIZE] > delay:
+                    reset_window_size()
+
+            if (WINDOW_SIZE > SSTHRESH):
+                temp_window_size_adder += 1/WINDOW_SIZE
+                if temp_window_size_adder >= 1:
+                    increase_window_size(seq_id)
+                    temp_window_size_adder -= 1.0
+            else:
+                increase_window_size(seq_id)
+
+        except socket.timeout:
+            # no ack received, resend unacked message
+            fast_retransmit = 0
+            #print("Socket Timeout")
+            reset_window_size()
+            resend_message(seq_id)
+        if (seq_id + MESSAGE_SIZE >= len(data)):
+            break
+
+    # send final closing message
+    send_closing_message(seq_id)
+
+    totalTime = (time.time() - StartThroughputTime)
+    totalPackages = int(len(data)/MESSAGE_SIZE) + (len(data) % MESSAGE_SIZE > 0)
+
+    per_packet_delay.popitem()
+    total = 0.0
+    count = 0
+    for value in per_packet_delay.values():
+        total += value
+        count += 1
+
+    # print("Total time was : " + totalTime.__str__())
+    # print("Total amount of packets : " + totalPackages.__str__())
+    
+    # print(round(len(data)/totalTime, 2).__str__() + ",")
+    # print(round(total/count, 2).__str__() + ",")
+    # print(round((len(data)/totalTime)/(total/count), 2).__str__())
+
+    print('{:.2f},'.format(round(len(data) / totalTime, 2)))
+    print('{:.2f},'.format(round(total / count, 2)))
+    print('{:.2f}'.format(round((len(data) / totalTime) / (total / count), 2)))
